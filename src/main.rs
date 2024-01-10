c2af10818b4ababaa2c0ade6a147dd16f610e79d
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-09 20:31:46 -0500

    zooming

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::ffi::CString;
 
 use raylib_sys::{
-    BeginDrawing, BeginMode3D, Camera3D, CameraProjection_CAMERA_ORTHOGRAPHIC,
-    ClearBackground, DrawCube, EndDrawing, EndMode3D, GetColor, InitWindow,
+    BeginDrawing, BeginMode3D, Camera3D, CameraProjection_CAMERA_PERSPECTIVE,
+    ClearBackground, DrawCube, EndDrawing, EndMode3D, GetColor, GetFrameTime,
+    InitWindow, IsKeyDown, KeyboardKey_KEY_S, KeyboardKey_KEY_W,
     WindowShouldClose,
 };
 
@@ -23,22 +24,30 @@ fn main() {
     let height = 600;
     let title = CString::new("Example").unwrap();
     let cube_size = 1.0;
-    let camera = Camera3D {
+    let camera_speed = cube_size;
+    let mut camera = Camera3D {
         position: vector3!(0.0, 0.0, -cube_size),
         target: vector3!(0.0, 0.0, 0.0),
         up: vector3!(0.0, 1.0, 0.0),
         fovy: 90.0,
-        projection: CameraProjection_CAMERA_ORTHOGRAPHIC as i32,
+        projection: CameraProjection_CAMERA_PERSPECTIVE as i32,
     };
     unsafe {
         InitWindow(width, height, title.as_ptr());
         let color = GetColor(0x181818AA);
         while !WindowShouldClose() {
+            let dt = GetFrameTime();
+            if IsKeyDown(KeyboardKey_KEY_W as i32) {
+                camera.position.z += camera_speed * dt;
+            }
+            if IsKeyDown(KeyboardKey_KEY_S as i32) {
+                camera.position.z -= camera_speed * dt;
+            }
             BeginDrawing();
             ClearBackground(color);
             BeginMode3D(camera);
             DrawCube(
-                vector3!(cube_size / 2.0, cube_size / 2.0, cube_size / 2.0),
+                vector3!(0.0, 0.0, 0.0),
                 cube_size,
                 cube_size,
                 cube_size,
