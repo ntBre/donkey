8c2aa148d69442126653088487b89d912255de13
Brent Westbrook <brentrwestbrook@gmail.com>
2024-01-27 12:06:35 -0500

    use safe api in main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-use std::ffi::CString;
-
-use donkey::vector3;
-use raylib_sys::{
-    BeginDrawing, BeginMode3D, Camera3D, CameraProjection_CAMERA_PERSPECTIVE,
-    ClearBackground, DrawCube, EndDrawing, EndMode3D, GetColor, GetFrameTime,
-    InitWindow, IsKeyDown, KeyboardKey_KEY_A, KeyboardKey_KEY_D,
-    KeyboardKey_KEY_S, KeyboardKey_KEY_W, WindowShouldClose,
-};
+use donkey::{colors::color, keys::Key, vector3, Window};
+use raylib_sys::Camera3D;
 
 /// basic example based on Tsoding's "Ridiculously Easy 3D in C" video:
 /// https://www.youtube.com/watch?v=K7hWqxC_7Mw
 fn main() {
     let width = 800;
     let height = 600;
-    let title = CString::new("Example").unwrap();
+    let title = "review";
     let cube_size = 1.0;
     let camera_speed = cube_size;
     let mut camera = Camera3D {
@@ -21,37 +14,35 @@ fn main() {
         target: vector3!(0.0, 0.0, 0.0),
         up: vector3!(0.0, 1.0, 0.0),
         fovy: 90.0,
-        projection: CameraProjection_CAMERA_PERSPECTIVE as i32,
+        projection: raylib_sys::CameraProjection_CAMERA_PERSPECTIVE as i32,
     };
-    unsafe {
-        InitWindow(width, height, title.as_ptr());
-        let color = GetColor(0x181818AA);
-        while !WindowShouldClose() {
-            let dt = GetFrameTime();
-            if IsKeyDown(KeyboardKey_KEY_W as i32) {
-                camera.position.z += camera_speed * dt;
-            }
-            if IsKeyDown(KeyboardKey_KEY_S as i32) {
-                camera.position.z -= camera_speed * dt;
-            }
-            if IsKeyDown(KeyboardKey_KEY_D as i32) {
-                camera.position.x += camera_speed * dt;
-            }
-            if IsKeyDown(KeyboardKey_KEY_A as i32) {
-                camera.position.x -= camera_speed * dt;
-            }
-            BeginDrawing();
-            ClearBackground(color);
-            BeginMode3D(camera);
-            DrawCube(
-                vector3!(0.0, 0.0, 0.0),
-                cube_size,
-                cube_size,
-                cube_size,
-                GetColor(0xff0000ff),
-            );
-            EndMode3D();
-            EndDrawing();
+    let win = Window::init(width, height, title);
+    let background = color(0x181818AA);
+    while !win.should_close() {
+        let dt = win.get_frame_time();
+        if win.is_key_down(Key::W) {
+            camera.position.z += camera_speed * dt;
         }
+        if win.is_key_down(Key::S) {
+            camera.position.z -= camera_speed * dt;
+        }
+        if win.is_key_down(Key::D) {
+            camera.position.x += camera_speed * dt;
+        }
+        if win.is_key_down(Key::A) {
+            camera.position.x -= camera_speed * dt;
+        }
+        win.begin_drawing();
+        win.clear_background(background);
+        win.begin_mode3d(camera);
+        win.draw_cube(
+            vector3!(0.0, 0.0, 0.0),
+            cube_size,
+            cube_size,
+            cube_size,
+            0xff0000ff,
+        );
+        win.end_mode3d();
+        win.end_drawing();
     }
 }
