a1afe04fd1a0cc1fe175d32ea1606a4190261c58
Brent Westbrook <brentrwestbrook@gmail.com>
2024-02-18 12:29:52 -0500

    add Vector3::new and Add and Sub impls

--- raylib-sys/src/vector3.rs
+++ raylib-sys/src/vector3.rs
@@ -1,4 +1,12 @@
+use std::ops::{Add, Sub};
+
 use crate::{Matrix, Vector3};
+
+impl Vector3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+}
 
 pub fn Vector3Add(v1: Vector3, v2: Vector3) -> Vector3 {
     Vector3 {
@@ -8,11 +16,27 @@ pub fn Vector3Add(v1: Vector3, v2: Vector3) -> Vector3 {
     }
 }
 
+impl Add<Vector3> for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, rhs: Vector3) -> Self::Output {
+        Vector3Add(self, rhs)
+    }
+}
+
 pub fn Vector3Subtract(v1: Vector3, v2: Vector3) -> Vector3 {
     Vector3 {
         x: v1.x - v2.x,
         y: v1.y - v2.y,
         z: v1.z - v2.z,
+    }
+}
+
+impl Sub<Vector3> for Vector3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Vector3) -> Self::Output {
+        Vector3Subtract(self, rhs)
     }
 }
 
